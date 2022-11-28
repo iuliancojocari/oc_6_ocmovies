33beab34f00724c56a874d53adda60f6c19cf580
IulianK <iuliancojocari42@gmail.com>
2022-11-28 22:53:10 +0100

    in progress

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,8 +1,47 @@
 const url = 'http://localhost:8000/api/v1/titles/'
-async function getBestMovies(skip, movies = 7, pagesize = '7') {
+
+async function getBestMovie() {
+  let bestMovie = document.querySelector('.best-movie')
+
+  let bestMovieDesc = document.querySelector('.best-movie__desc')
+  let movieTitle = document.createElement('h1')
+  movieTitle.classList.add('best-movie__title')
+
+  let bestMovieCover = document.createElement('div')
+  bestMovieCover.classList.add('best-movie__cover')
+
+  let img = document.createElement('img')
+  let desc = document.createElement('p')
+
+  try {
+    const response = await fetch(url + '?sort_by=-imdb_score')
+
+    if (response.ok) {
+      const data = await response.json()
+      movieTitle.innerHTML = data['results'][0]['title']
+      bestMovieDesc.insertBefore(movieTitle, bestMovieDesc.firstChild)
+
+      img.src = data['results'][0]['image_url']
+
+      const descResponse = await fetch(data['results'][0]['url'])
+      const descData = await descResponse.json()
+
+      desc.innerHTML = descData['description']
+      movieTitle.after(desc)
+
+      bestMovieCover.appendChild(img)
+      bestMovie.appendChild(bestMovieCover)
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+getBestMovie()
+
+async function getBestMovies(category, skip, movies = 7, pagesize = '7') {
   try {
     const response = await fetch(
-      url + '?sort_by=-imdb_score&page_size=' + pagesize
+      url + '?sort_by=-imdb_score&page_size=' + pagesize + '&genre=' + category
     )
 
     if (response.ok) {
@@ -20,12 +59,11 @@ async function getBestMovies(skip, movies = 7, pagesize = '7') {
       return moviesData
     }
   } catch (error) {
-    alert(error)
     console.log(error)
   }
 }
 
-async function createCarousel(skip = 0) {
+async function createCarousel(category, categoryName, skip = 0) {
   // create section element with class categories
   let section = document.createElement('section')
   section.classList.add('categories')
@@ -39,6 +77,7 @@ async function createCarousel(skip = 0) {
 
   // create title element with class category title
   let title = document.createElement('h2')
+  title.innerHTML = categoryName + ' movies'
   title.classList.add('category-title')
   carousel.appendChild(title)
 
@@ -50,10 +89,9 @@ async function createCarousel(skip = 0) {
   let carouselContent = document.createElement('div')
   carouselContent.classList.add('carousel-content')
 
-  let moviesData = await getBestMovies(skip)
+  let moviesData = await getBestMovies(category, skip)
 
   for (movie of moviesData) {
-    console.log(movie)
     // create carousel card
     let card = document.createElement('div')
     card.classList.add('card')
@@ -74,7 +112,6 @@ async function createCarousel(skip = 0) {
   nextButton.addEventListener('click', () => {
     carouselContent.scrollLeft += 272
   })
-  carouselContainer.appendChild(nextButton)
 
   // left button
   const previousButton = document.createElement('button')
@@ -84,14 +121,18 @@ async function createCarousel(skip = 0) {
   previousButton.addEventListener('click', () => {
     carouselContent.scrollLeft -= 272
   })
-  carouselContainer.appendChild(previousButton)
 
   carouselContainer.appendChild(carouselContent)
+  carouselContainer.appendChild(nextButton)
+  carouselContainer.appendChild(previousButton)
 
   carousel.appendChild(carouselContainer)
   section.appendChild(carousel)
 }
 
 window.addEventListener('load', function () {
-  createCarousel((skip = 1))
+  createCarousel((category = ''), (categoryName = 'Best'), (skip = 1))
+  createCarousel((category = 'drama'), (categoryName = 'Drama'))
+  createCarousel((category = 'family'), (categoryName = 'Family'))
+  createCarousel((category = 'romance'), (categoryName = 'Romance'))
 })
